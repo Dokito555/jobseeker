737318d5b58e5d9259f7351e04c4bfc79c2f72b5
Dokito555 <thinkingwhiterabbit@gmail.com>
2025-12-18 20:20:16 +0700

    fixed smthng

--- lib/data/model/user_model.g.dart
+++ lib/data/model/user_model.g.dart
@@ -10,26 +10,26 @@ UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
   id: (json['id'] as num).toInt(),
   email: json['email'] as String,
   name: json['name'] as String,
-  phoneNumber: json['phoneNumber'] as String,
+  phoneNumber: json['phone_number'] as String,
   skills: (json['skills'] as List<dynamic>)
       .map((e) => (e as num).toInt())
       .toList(),
   token: json['token'] as String,
-  refreshToken: json['refreshToken'] as String,
-  createdAt: json['createdAt'] as String,
-  updatedAt: json['updatedAt'] as String,
+  refreshToken: json['refresh_token'] as String,
+  createdAt: json['created_at'] as String,
+  updatedAt: json['updated_at'] as String,
 );
 
 Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
   'id': instance.id,
   'email': instance.email,
   'name': instance.name,
-  'phoneNumber': instance.phoneNumber,
+  'phone_number': instance.phoneNumber,
   'skills': instance.skills,
   'token': instance.token,
-  'refreshToken': instance.refreshToken,
-  'createdAt': instance.createdAt,
-  'updatedAt': instance.updatedAt,
+  'refresh_token': instance.refreshToken,
+  'created_at': instance.createdAt,
+  'updated_at': instance.updatedAt,
 };
 
 UserRegisterRequest _$UserRegisterRequestFromJson(Map<String, dynamic> json) =>
@@ -37,9 +37,9 @@ UserRegisterRequest _$UserRegisterRequestFromJson(Map<String, dynamic> json) =>
       email: json['email'] as String,
       password: json['password'] as String,
       name: json['name'] as String,
-      phoneNumber: json['phoneNumber'] as String,
+      phoneNumber: json['phone_number'] as String,
       address: json['address'] as String,
-      skillIds: (json['skillIds'] as List<dynamic>)
+      skillIds: (json['skill_ids'] as List<dynamic>)
           .map((e) => (e as num).toInt())
           .toList(),
     );
@@ -50,9 +50,9 @@ Map<String, dynamic> _$UserRegisterRequestToJson(
   'email': instance.email,
   'password': instance.password,
   'name': instance.name,
-  'phoneNumber': instance.phoneNumber,
+  'phone_number': instance.phoneNumber,
   'address': instance.address,
-  'skillIds': instance.skillIds,
+  'skill_ids': instance.skillIds,
 };
 
 UserLoginRequest _$UserLoginRequestFromJson(Map<String, dynamic> json) =>
